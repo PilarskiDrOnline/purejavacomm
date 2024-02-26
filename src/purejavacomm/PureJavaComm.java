91f48f35d74cfc44e18b45bc2da190600f6bf91b
Karol Pilarski <karol.pilarski@dr-online.local>
2024-02-26 15:18:01 +0100

    init, gradle with updated JNA and Java, Maven files and scripts deleted,

--- src/purejavacomm/PureJavaComm.java
+++ src/purejavacomm/PureJavaComm.java
@@ -3,12 +3,12 @@ package purejavacomm;
 public class PureJavaComm {
 	// change this if you modify the code and re-distribute
 	public static String getVersion() {
-		return "1.0.3";
+		return "1.0.0";
 	}
 
 	// change this is if you fork the project
 	public static String getFork() {
-		return "SpareTimeLabs";
+		return "Dr-Online";
 	}
 
 }
